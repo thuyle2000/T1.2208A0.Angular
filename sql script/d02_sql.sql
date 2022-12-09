d50442e276e310ed97cd3763732cdb1d88e046b0
Le Mong Thuy <thuyle2000@gmail.com>
2022-12-09 15:58:57 +0700

    Upload Dec 09, 2022

---
+++ sql script/d02_sql.sql
@@ -0,0 +1,37 @@
+-- open database db2208_A0
+use db2208_A0
+go
+
+-- xem/lietke/truy van ds tat ca cac sinh vien
+select * from tbStudent
+
+/* truy van du lieu voi cac vi tu LIKE, IN, BETWEEN */
+-- tim cac sinh vien co ho 'vo' : ap dung vi tu (predicate) LIKE
+select * from tbStudent where st_name LIKE 'vo%'
+
+-- tim cac sv co ten dem la 'van'
+select * from tbStudent where st_name LIKE '% van %'
+
+-- tim cac sv co ten bat dau la chu 't'
+-- in ra ten cua cac sinh vien
+select *,LEN(st_name), LEN(st_name) - CHARINDEX(' ',REVERSE(st_name)) from tbStudent
+
+select *,LEN(st_name), 
+	LEN(st_name) - CHARINDEX(' ',REVERSE(st_name)), 
+	SUBSTRING(st_name,LEN(st_name) - CHARINDEX(' ',REVERSE(st_name))+2, LEN(st_name))
+from tbStudent
+
+--tim cac sv sinh nam 2000-2003
+select * from tbStudent where YEAR(dob)>=2000 and YEAR(dob)<=2003
+
+--tim cac sv sinh nam 2000-2003: ap dung vi tu BETWEEN .. AND ..
+select * from tbStudent where YEAR(dob) BETWEEN 2000 AND 2003
+
+-- tim cac sv thuoc nhom cua leader co ma so 's01' va 's07'
+select * from tbStudent 
+	where leader_id LIKE 's01' OR leader_id LIKE 's07'
+	      OR st_id LIKE 's01' OR st_id LIKE 's07'
+-- tim cac sv thuoc nhom cua leader co ma so 's01' va 's07' : ap dung vi tu IN
+select * from tbStudent 
+	where leader_id IN ('s01','s07') OR st_id IN ('s01','s07')
+GO
