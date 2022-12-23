14fad8502a8ec77d17ec9d80f45ce6b41eb88fc4
Le Mong Thuy <thuyle2000@gmail.com>
2022-12-23 15:28:38 +0700

    Upload Dec 23, 2022

--- sql script/d07_index.sql
+++ sql script/d07_index.sql
@@ -1,4 +1,4 @@
-use db2208_A0
+﻿use db2208_A0
 go
 
 select * from tbEmployee
@@ -6,4 +6,37 @@ go
 
 -- tao clustered index tren cot ho ten nhan vien
 create clustered index ix_employee_name on tbEmployee(e_name)
+go
+
+-- them 3 nv voi 
+insert tbEmployee values 
+(N'Đỗ Anh Dũng', '2004-08-12',1, 1500),
+(N'Lý Lan', '1958-02-12',0,3400),
+(N'Đoàn Quỳnh Anh', '1963-05-24', 0, 2340)
+
+-- xem ds nhan vien
+select * from tbEmployee
+go
+
+-- xem danh sach cac mon hoc
+select * from tbModule
+go
+
+-- tao index tren cot ten mon hoc
+create index ix_module_name on tbmodule(module_name desc)
+go
+
+-- xem danh sach cac mon hoc
+select * from tbModule
+go
+
+-- them 1 mon hoc moi
+insert tbModule values ('M07','Design, Development DB', 8, 0)
+
+-- them 1 mon hoc nua
+insert tbModule values ('M06','XML and JSON', 20, 140)
+go
+ 
+-- tao 1 filter index tren cot hoc phi (dk so gio hoc >= 20)
+create index ix_fee on tbModule(fee) where hours>=20
 go
