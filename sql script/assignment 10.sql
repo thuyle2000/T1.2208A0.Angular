66b72fdb01d41955e169523677ee109eee98754b
Le Mong Thuy <thuyle2000@gmail.com>
2022-12-28 15:13:15 +0700

    Upload Dec 28, 2022

--- sql script/assignment 10.sql
+++ sql script/assignment 10.sql
@@ -17,4 +17,42 @@ log on
 go
 
 -- open db
-use ass1
+use Ass10_Db
+go
+
+/*
+Table: tbCustomer
+Column Names Data type Description
+CustCode VARCHAR(5) Customer Code – PRIMARY KEY
+CustName VARCHAR(30) Customer Name – NOT NULL
+CustAddress VARCHAR(50) Customer Address – NOT NULL
+CustPhone VARCHAR(15) Customer Phone
+CustEmail VARCHAR(25) Email address of customer
+CustStatus VARCHAR(10) Status of the Customer either Valid or Invalid, default value is Valid
+*/
+create table tbCustomer (
+	CustCode VARCHAR(5) not null primary key nonclustered,
+	CustName VARCHAR(30) not null,
+	CustAddress VARCHAR(50) not null,
+	CustPhone VARCHAR(15),
+	CustEmail VARCHAR(25),
+	CustStatus VARCHAR(10) not null DEFAULT 'Valid'
+					CHECK( CustStatus in ('Valid','Invalid') )
+)
+go
+
+/*
+Table: tbMessage
+Column Names Data type Description
+MsgNo INTEGER Customer message number – identity (1000,1) - PRIMARY KEY
+CustCode VARCHAR(5) Customer Code – FOREIGN KEY REF
+MsgDetails VARCHAR(300) Customer message details – NOT NULL
+MsgDate DATETIME Date of message – NOT NULL, default value is current date
+Status VARCHAR(10) Status of message either Pending or Resolved
+*/
+create table tbMessage (
+	MsgNo int identity(1000,1) primary key NONCLUSTERED,
+	CustCode VARCHAR(5) FOREIGN KEY References tbCustomer(CustCode),
+	MsgDetails DATE not null DEFAULT GETDATE(),
+	[Status] VARCHAR(10) not null CHECK ([status] IN ('Pending', 'Resolved') )
+)
